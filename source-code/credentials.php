51f6cca1d6c60124036b5ad3bccb8934458b56f6
tankwhitman <92003688+tankwhitman@users.noreply.github.com>
2022-02-22 20:44:58 -0600

    remove some sensitive data
    data has now been changed

--- source-code/credentials.php
+++ source-code/credentials.php
@@ -1,7 +1,7 @@
 <?php
 // Info from Heroku dashboard
-$hn = 'us-cdbr-east-05.cleardb.net';
-$un = 'b1b47ba60e5e26';
-$pw = '3c28e0f0';
+$hn = '####';
+$un = '####';
+$pw = '####';
 $db = 'heroku_d72d6317a6f4d86';
 ?>

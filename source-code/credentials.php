283aa52a39ced75bb5049be454de5a977454e6a5
Addison Green <addisongreen99@gmail.com>
2022-02-22 19:36:45 -0600

    Update credentials file to use environment variables

--- source-code/credentials.php
+++ source-code/credentials.php
@@ -1,7 +1,7 @@
 <?php
 // Info from Heroku dashboard
-$hn = 'us-cdbr-east-05.cleardb.net';
-$un = 'b1b47ba60e5e26';
-$pw = '3c28e0f0';
-$db = 'heroku_d72d6317a6f4d86';
+$hn = getenv("DATABASE_URL");
+$un = getenv("DATABASE_USERNAME");
+$pw = getenv("DATABASE_PASSWORD");
+$db = getenv("DATABASE_NAME");
 ?>

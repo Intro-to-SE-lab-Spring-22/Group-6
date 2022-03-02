3d5ab6b740c8a424d8103402f6494105d833e948
Addison Green <addisongreen99@gmail.com>
2022-03-01 19:15:04 -0600

    Add account login functionality Fixes #3

--- source-code/index.php
+++ source-code/index.php
@@ -3,8 +3,51 @@
 <head>
   <meta charset="utf-8">
   <title>Default Title</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="ajax.js"></script>
 </head>
+
 <body>
-  <?="Default Body Text"?>
+  <form onsubmit="return false">
+    <div class="row">
+      Username<br><input type="text" id="username"><br>
+    </div>
+    <div class="row">
+      Password<br><input type="password" id="password"><br>
+    </div>
+    <div class="row">
+      <input type="submit" id="submit" value="Log In" onclick="submitButton()">
+    </div>
+  </form>
+  <div id="placeholder"></div>
 </body>
 </html>
+
+<script>
+
+function submitButton() {
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+
+  $.post(
+    "login.php",
+    {
+      username: username,
+      password: password
+    },
+    function(result) {
+      var json = JSON.parse(result);
+
+      if (json.success == "true") {
+        document.location = json.location;
+      }
+      else {
+        document.getElementById('placeholder').innerHTML = "Your username or password is incorrect";
+      }
+    }
+  );
+
+}
+
+</script>

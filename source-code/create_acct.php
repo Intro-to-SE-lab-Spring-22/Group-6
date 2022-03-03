f03334ebef81e019c62e82522a8ab68bd1e04a6f
Joshua <tankwhitman@gmail.com>
2022-03-02 22:44:09 -0600

    Large Update which focuses on splash page and log in
    Stylized CSS for log in screen and Create account page. Beginning of the start to create account php backend stuff. Also organized another 2 folders in order for us to keep login and creation stuff seperate in order to keep the DB safe

---
+++ source-code/create_acct.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Create Account</title>
+    <!-- jQuery + Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script> 
+</head>
+<body>
+    <div class="App">
+        <div class="vertical-center">
+            <div class="inner-block">
+                <form action="" method="post">
+                    <h3>Register</h3>
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" class="form-control" name="username" id="username" />
+                    </div>
+                    <div class="form-group">
+                        <label>First name</label>
+                        <input type="text" class="form-control" name="firstname" id="firstName" />
+                    </div>
+                    <div class="form-group">
+                        <label>Last name</label>
+                        <input type="text" class="form-control" name="lastname" id="lastName" />
+                    </div>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input type="email" class="form-control" name="email" id="email" />
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" name="password" id="password" />
+                    </div>
+                    <button type="submit" name="submit" id="submit" class="btn btn-outline-primary btn-lg btn-block">
+                        Sign up
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- source-code/index.php
+++ source-code/index.php
@@ -1,25 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <title>Default Title</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Login</title>
+  <!-- jQuery + Bootstrap JS -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="ajax.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </head>
 
 <body>
-  <form onsubmit="return false">
-    <div class="row">
-      Username<br><input type="text" id="username"><br>
+  <div class="App">
+    <div class="vertical-center">
+      <div class="inner-block">
+        <h3>Log In</h3>  
+          <form onsubmit="return false">
+          <div class="form-group">
+            <label for="username">Username</label>  
+            <input type="text" name="username" id="username"><br>
+          </div>
+          <div class="form-group">
+            <label>Password</label>
+            <input type="password" id="password"><br>
+          </div>
+          <button type="submit" id="submit" value="Log In" class="btn btn-outline-primary btn-lg btn-block" onclick="submitButton()">
+            Log In
+          </button>
+          <button type="submit" name="submit" class="btn btn-outline-primary btn-lg btn-block">
+            Sign Up
+          </button>
+          
+      </div>
     </div>
-    <div class="row">
-      Password<br><input type="password" id="password"><br>
-    </div>
-    <div class="row">
-      <input type="submit" id="submit" value="Log In" onclick="submitButton()">
-    </div>
-  </form>
-  <div id="placeholder"></div>
+  </div>
+</form>
+    <div id="placeholder"></div>
+   
 </body>
 </html>
 
@@ -37,6 +54,7 @@ function submitButton() {
       password: password
     },
     function(result) {
+      document.getElementById('placeholder').innerHTML = result;
       var json = JSON.parse(result);
 
       if (json.success == "true") {
@@ -50,4 +68,10 @@ function submitButton() {
 
 }
 
+
 </script>
+
+
+
+
+

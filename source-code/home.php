6354d37092d06a1ac53f0ec4380e89e36ab9f581
Addison Green <addisongreen99@gmail.com>
2022-04-26 19:05:32 -0500

    quick js include fix

--- source-code/home.php
+++ source-code/home.php
@@ -7,6 +7,7 @@
   <title>Home</title>
   <!-- jQuery + Bootstrap JS -->
   <script src="js/functions.js"></script>
+  <script src="js/post_requests.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
   <script src="https://kit.fontawesome.com/c56bd8cfd4.js" crossorigin="anonymous"></script>
